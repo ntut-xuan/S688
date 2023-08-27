dab0d73fd45c54257c96b9f422d9d7b2693b7fbd
ntut-xuan <t109590031@ntut.org.tw>
2023-08-27 23:35:22 +0800

    Setup loguru format

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+import sys
 from dataclasses import dataclass
 from http import HTTPMethod
 from typing import Any
@@ -27,7 +28,14 @@ def fetch_route(swagger_flat_data: dict[str, Any]):
     logger.info(f"Fetch {len(routes)} routes.")
 
 
+def setup_loguru_format() -> None:
+    logger.remove()
+    logger.add(sys.stderr, format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> [<level>{level:^7s} </level>] - <level>{message}</level>")
+
 if __name__ == "__main__":
+
+    setup_loguru_format()
+
     swagger_data: dict[str, Any] = load_swagger_file("swagger.json")
     schema_components: dict[str, Any] = load_swagger_component(swagger_data, ComponentType.SCHEMA)
     response_components: dict[str, Any] = load_swagger_component(swagger_data, ComponentType.RESPONSE)
